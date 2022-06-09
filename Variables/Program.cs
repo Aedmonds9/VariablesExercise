2aa47297cc7695331aafc805bf2b54a8feb0ec0d
Andrew Edmonds <edmondsandrew9@gmail.com>
2022-06-08 23:51:23 -0400

    initial commit

---
+++ Variables/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string customerName = "Steven";
+
+           int customerNumber = 120;
+
+            bool isCustomer;
+
+            bool isEmployee;
+
+            isCustomer = true;
+
+            isEmployee = false;
+
+             char = '8';
+
+            Console.WriteLine(Convert.ToDouble(customerName));
+            
+            Console.WriteLine ($"Hello, {customerName} you are customer number {customerNumber}.");
+
+            
+            
+
+
+
+
+        }
+    }
+}
